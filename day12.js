01e0ce15154efd5134e35886003e58d071cb9334
talarisulochana <talarisulochana123@gmail.com>
2024-11-02 18:56:51 +0530

    Add files via upload

---
+++ day12.js
@@ -0,0 +1,41 @@
+var a =20;
+let a1=200;
+const a2=40;
+
+{
+    let a1=200;
+    const a2=40;
+    console.log(a1+a2);
+}
+
+function A(){
+ 
+    let a1="Chinni";
+    const a2=21; 
+    console.log(a1+a2)  
+}
+A()
+
+var b=400;
+function b(){
+    let a=10;
+    var c=30;
+    console.log(a+b+c)
+}
+
+function a3(){
+    var z=300;
+    console.log(z)
+}
+a3()
+
+let d=100;
+function e(){
+    var e=200;
+    function b1(){
+    let b1=200;
+    console.log(d+e+b1);
+    }
+    b1()
+}
+e()
